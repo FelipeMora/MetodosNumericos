827c83c3ae5b5f337c8f44df32a931bb7e529430
FelipeMora <fmora51@ctb.edu.co>
2017-10-04 16:08:47 -0500

    Cambios (Realizados para mejorar estructura)
    Se realizarón cambios en los comentarios en la implementación de la interfaz con Tkinter

--- SerieDeTaylor.py
+++ SerieDeTaylor.py
@@ -3,19 +3,18 @@
 from tkinter import *
 def Derivada():
     Derivar = FuncionGET.get()
-    text = "Procesando derivada... "
-    if (Derivar == ""):
-        text = "No ha ingresado derivada!"
-    if (text == "Procesando derivada... "):
+    Show = "Procesando derivada... "
+    if Derivar == "":
+        Show = "No ha ingresado derivada!"
+    else:
         VentanaProcDeriv.deiconify()
         ValReal(Derivar)
-    MosDerivada = Label(ventana,text=text + Derivar,fg="red",font=("Agency FB",14)).place(x=20,y=60)
+    MosDerivada = Label(ventana,text=Show + Derivar,fg="red",font=("Agency FB",14)).place(x=20,y=60)
 
 def ValReal(Derivar):
-    Xi = IntVar()
     print("Sacar el valor real de " + Derivar)
     #Creacion de un nuevo LabelFrame
-    LFrame2 = LabelFrame(LFrame,padx=10,pady=10,cursor="arrow")
+    LFrame2 = LabelFrame(ventana, text="Serie de Taylor",padx=10,pady=10,cursor="arrow")
     LFrame2.pack(fill="both",expand="yes")
     LFrame2.config(width=50)
     #Widgets pertenecientes a el LabelFrame
@@ -23,9 +22,10 @@ def ValReal(Derivar):
     lbValXi = Label(LFrame2,text="Xi = ",font=("Agency FB",14)).place(x=130,y=80)
     campoGETXi = Entry(LFrame2,textvariable=Xi).place(x=230,y=80)
 
+#Creating windows
 ventana = Tk()
-ventana.geometry("500x600")
-ventana.title("Serie de Taylor")
+ventana.geometry("500x600+50+50")
+ventana.title("Métodos Numéricos")
 
 VentanaProcDeriv = Toplevel(ventana)
 VentanaProcDeriv.title("Derivaciones")
@@ -35,17 +35,18 @@ VentanaProcDeriv.withdraw()
 LFrame = LabelFrame(ventana, text="Opciones",padx=10,pady=10)
 LFrame.pack(fill="both",expand="yes")
 
+Xi = IntVar()
 FuncionGET = StringVar()
 
 Etiqueta = Label(LFrame,text="Ingrese la funcion: ",font=("Agency FB",14)).place(x=10,y=0)
 campoGET = Entry(LFrame,textvariable=FuncionGET).place(x=130,y=10)
 botonGETFun = Button(LFrame,text="Enviar",width=7, height=0,cursor="hand1",relief="groove",command=Derivada).place(x=260,y=5)
-#Pertenecen a VentanaProcDeriv
+#Windows of derived
 LabelTitleLis = Label(VentanaProcDeriv,text="Derivadas",font=("Agency FB",14)).place(x=10,y=0)
-lstDeriv = Listbox(VentanaProcDeriv, width=50)
-#Agregar A la lista
+lstDeriv = Listbox(VentanaProcDeriv, width=60)
+#Add to list
 lstDeriv.insert(1,"-2x^3");
-#Termina de agregar
+#End of add
 lstDeriv.place(x=10,y=30)
-
+#Show the windows
 ventana.mainloop();
